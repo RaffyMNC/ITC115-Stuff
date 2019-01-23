df968167a47bc94890b9a6b1e316d97369098649
RaffyMNC <46461374+RaffyMNC@users.noreply.github.com>
2019-01-23 11:00:45 -0800

    Powers of a number!

---
+++ assignment3.java
@@ -0,0 +1,29 @@
+
+public class assignment3 {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(printPowersOfN(4, 3));
+		System.out.println(printPowersOfN(5, 6));	
+		System.out.println(printPowersOfN(-2, 8));
+		System.out.println(printPowersOfN(9,9));
+		
+	}
+		
+		public static int printPowersOfN(int b, int e) {
+			
+			int number=1;
+			for (int i=0; i <e; i++) {
+				System.out.print((int)Math.pow(b, i)+" ");
+				number*=b;
+				
+			
+		}
+			return number;
+	
+		}
+}
+
+
+
